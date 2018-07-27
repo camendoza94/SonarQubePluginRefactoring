c66b905d9681efad76b51c5119ec01699b030e87
Camilo Mendoza <ca.mendoza968@uniandes.edu.co>
2018-07-27 18:33:46 -0500

    Added sample of diagram on SQ page.

--- src/main/js/components/VersionsMeasuresHistoryApp.js
+++ src/main/js/components/VersionsMeasuresHistoryApp.js
@@ -6,7 +6,35 @@
 import React from 'react';
 import MeasuresHistory from './MeasuresHistory'
 import {translate} from '../common/l10n.js'
-import {findVersionsAndMeasures} from '../api.js'
+import {findVersionsAndMeasures, findProjects} from '../api.js'
+import {Line} from 'react-chartjs-2';
+
+const data = {
+    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+    datasets: [
+        {
+            label: 'My First dataset',
+            fill: false,
+            lineTension: 0.1,
+            backgroundColor: 'rgba(75,192,192,0.4)',
+            borderColor: 'rgba(75,192,192,1)',
+            borderCapStyle: 'butt',
+            borderDash: [],
+            borderDashOffset: 0.0,
+            borderJoinStyle: 'miter',
+            pointBorderColor: 'rgba(75,192,192,1)',
+            pointBackgroundColor: '#fff',
+            pointBorderWidth: 1,
+            pointHoverRadius: 5,
+            pointHoverBackgroundColor: 'rgba(75,192,192,1)',
+            pointHoverBorderColor: 'rgba(220,220,220,1)',
+            pointHoverBorderWidth: 2,
+            pointRadius: 1,
+            pointHitRadius: 10,
+            data: [65, 59, 80, 81, 56, 55, 40]
+        }
+    ]
+};
 
 export default class VersionsMeasuresHistoryApp extends React.PureComponent {
 
@@ -15,12 +43,19 @@ export default class VersionsMeasuresHistoryApp extends React.PureComponent {
   };
 
   componentDidMount() {
-    findVersionsAndMeasures(this.props.project).then(
+    /*findVersionsAndMeasures(this.props.project).then(
       (valuesReturnedByAPI) => {
         this.setState({
           data: valuesReturnedByAPI
         });
       }
+    );*/
+    findProjects().then(
+        (projectData) => {
+          this.setState({
+              data: projectData
+          });
+        }
     );
   }
 
@@ -28,6 +63,7 @@ export default class VersionsMeasuresHistoryApp extends React.PureComponent {
     // Data Gathered: {JSON.stringify(this.state.data)}
     return (
       <div className="page page-limited">
+          <Line data={data} />
         <table className="data zebra">
           <thead><tr className="code-components-header">
             <th className="thin nowrap text-left code-components-cell">Version</th>
